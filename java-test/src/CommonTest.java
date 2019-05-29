d11c5b5950b857abb722228526190394a56258be
admin <fu_zhongyuan@163.com>
2019-05-29 19:50:42 +0800

    测试git commit

---
+++ java-test/src/CommonTest.java
@@ -0,0 +1,12 @@
+import java.util.Optional;
+
+public class CommonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "";
+		Optional optional = Optional.ofNullable(a);
+//		optional.map(1).orElse(0);
+	}
+
+}
---
+++ nexus-test/src/main/java/com/nexus/test/NexusTest.java
@@ -0,0 +1,8 @@
+package com.nexus.test;
+
+public class NexusTest {
+
+	public static void main(String[] args) {
+		System.out.println("nexus test java ...");
+	}
+}
